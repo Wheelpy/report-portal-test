d24d8a2f7945cdad498eb0f974a191ebee94e407
Wheelpy <wheelpy@mail.ru>
2025-09-30 21:34:28 +0400

    fix config not receiving variables

--- utils/config.ts
+++ utils/config.ts
@@ -1,4 +1,6 @@
+import "dotenv/config";
+
 export const config = {
-  username: process.env.RP_USER || "default_user",
-  password: process.env.RP_PASS || "default_pass",
+  username: process.env.RP_USER,
+  password: process.env.RP_PASS,
 };
